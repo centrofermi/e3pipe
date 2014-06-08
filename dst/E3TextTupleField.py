04732b8b65d91453321547bd718698b7c883d94d
lbaldini luca baldini <lbaldini luca.baldini@pi.infn.it>
2014-06-08 08:23:18 +0200

    Initial import.

---
+++ dst/E3TextTupleField.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# *********************************************************************
+# * Copyright (C) 2014 Luca Baldini (luca.baldini@pi.infn.it)         *
+# *                                                                   *
+# * For the license terms see the file LICENCE, distributed           *
+# * along with this software.                                         *
+# *********************************************************************
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+
+class E3TextTupleField:
+
+    """ Small utility class describing a field in a text tuple.
+
+    This encapulates the name, type and units of the field.
+    """
+
+    def __init__(self, fieldName, fieldType, units = None):
+        """ Constructor.
+        """
+        self.Name = fieldName
+        self.Type = fieldType
+        self.Units = units
+
