c7224b52c806b1fea0150c0d872b079ef4c20cb5
Luca Baldini <luca.baldini@pi.infn.it>
2014-06-11 19:30:12 +0200

    Paths to the executables updated.

--- __package__.py
+++ __package__.py
@@ -28,7 +28,7 @@ import os
 E3PIPE_BASE = os.path.abspath(os.path.dirname(__file__))
 E3PIPE_APPS = os.path.join(E3PIPE_BASE, 'apps')
 E3PIPE_DST = os.path.join(E3PIPE_BASE, 'dst')
-E3PIPE_RECON = os.path.join(E3PIPE_BASE, 'recon')
+E3PIPE_RECON = os.path.join(E3PIPE_BASE, 'recon', 'linux')
 E3PIPE_ROOT = os.path.join(E3PIPE_BASE, 'ROOT')
 E3PIPE_TASKS = os.path.join(E3PIPE_BASE, 'tasks')
 E3PIPE_TEST = os.path.join(E3PIPE_BASE, 'test')
@@ -36,7 +36,7 @@ E3PIPE_TEST = os.path.join(E3PIPE_BASE, 'test')
 """ Paths to the analysis program executables. 
 """
 E3FORTRAN_ANALYZER_PATH = os.path.join(E3PIPE_RECON, 'fortran', 'EEE_V15')
-E3CPP_ANALYZER_PATH = os.path.join(E3PIPE_RECON, 'cpp', 'EEE_V15')
+E3CPP_ANALYZER_PATH = os.path.join(E3PIPE_RECON, 'cpp', 'EEE_Analyzer')
 
 """ Name of the calibration file that is created on the fly by the analysis
 program.
