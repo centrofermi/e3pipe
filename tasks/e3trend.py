a174a56c3bd4c8d18afced39f1a8558bcc30723a
Luca Baldini <luca.baldini@pi.infn.it>
2014-10-09 22:47:09 +0200

    Initial import.

---
+++ tasks/e3trend.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# *********************************************************************
+# * Copyright (C) 2014 Luca Baldini (luca.baldini@pi.infn.it)         *
+# *                                                                   *
+# * For the license terms see the file LICENCE, distributed           *
+# * along with this software.                                         *
+# *********************************************************************
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+import ROOT
+
+from e3pipe.root.E3InputRootFile import E3InputRootFile
+
+
+def e3trend(dstFilePath, timeBin = 300):
+    """ Read the dst anc create the trending quantities.
+    """
+    dstFile = E3InputRootFile(dstFilePath)
+    return dstFilePath
+
+
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    (opts, args) = parser.parse_args()
+    for arg in args:
+        e3trend(arg)
