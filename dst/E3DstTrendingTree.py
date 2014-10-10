b0d25a8795f1273cc1946f15ad69df3d1ea3e3e5
Luca Baldini <luca.baldini@pi.infn.it>
2014-10-10 10:03:09 +0200

    Initial import.

---
+++ dst/E3DstTrendingTree.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# *********************************************************************
+# * Copyright (C) 2014 Luca Baldini (luca.baldini@pi.infn.it)         *
+# *                                                                   *
+# * For the license terms see the file LICENCE, distributed           *
+# * along with this software.                                         *
+# *********************************************************************
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+from e3pipe.root.E3Tree import E3Tree
+from e3pipe.root.E3BranchDescriptor import E3BranchDescriptor
+
+
+class E3DstTrendingTree(E3Tree):
+    
+    """ Class describing the ROOT tree containing all the trending information.
+    """
+
+    NAME = 'Trending'
+    BRANCHES = [E3BranchDescriptor('RunNumber', 'i'),
+                E3BranchDescriptor('BinStart', 'F'),
+                E3BranchDescriptor('BinEnd', 'F'),
+                E3BranchDescriptor('RateTrackEvents', 'F'),
+                E3BranchDescriptor('RateTrackEventsErr', 'F'),
+            ]
+
+    def __init__(self):
+        """ Constructor.
+        """
+        E3Tree.__init__(self, 'Trending tree')
+
+
+
+def test():
+    """
+    """
+    tree = E3DstTrendingTree()
+
+
+
+if __name__ == '__main__':
+    test()
---
+++ root/E3TreePlotter.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# *********************************************************************
+# * Copyright (C) 2014 Luca Baldini (luca.baldini@pi.infn.it)         *
+# *                                                                   *
+# * For the license terms see the file LICENCE, distributed           *
+# * along with this software.                                         *
+# *********************************************************************
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+from e3pipe.__logging__ import logger, abort
+from e3pipe.root.E3H1D import E3H1D
+
+
+class E3TreePlotter:
+
+    """ Base class for making plots out of a root tree.
+    """
+
+    def __init__(self):
+        """ Constructor.
+        """
+        self.__PlotDict = {}
+
+    def plots(self):
+        """ Return all the underlying plot objects.
+        """
+        return self.__PlotDict.values()
+
+    def hist1d(self, expression, cut = '', xmin = None, xmax = None,
+               xbins = 100, xpad = 0, **kwargs):
+        """ Create a 1-dimensional histogram.
+        """
+        logger.info('Creating 1-d histogram for %s...' % expression)
+        kwargs['XTitle'] = kwargs.get('XTitle', expression)
+        kwargs['YTitle'] = kwargs.get('YTitle', 'Entries/bin')
+        hname = 'h%s' % expression
+        if xmin is None or xmax is None:
+            _xmin = self.GetMinimum(expression)
+            _xmax = self.GetMaximum(expression) 
+            _xrange = _xmax - _xmin
+        if xmin is None:
+            xmin = _xmin - xpad*_xrange
+        if xmax is None:
+            xmax = _xmax + xpad*_xrange
+        htitle = expression
+        hist = E3H1D(hname, htitle, xbins, xmin, xmax, **kwargs)
+        self.Project(hname, expression, cut)
+        self.__PlotDict[hist.GetName()] = hist
+        return hist
+
+    def hist2d(self, expression, cut = '', **kwargs):
+        """ Create a 2-dimensional histogram.
+        """
+        pass
