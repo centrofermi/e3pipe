7f5b72ef661303ded81a9dfe17322b64e66ee1eb
Luca Baldini <luca.baldini@pi.infn.it>
2014-10-13 22:36:54 +0200

    Initial import.

---
+++ dqm/E3DataQualityMonitor.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# *********************************************************************
+# * Copyright (C) 2014 Luca Baldini (luca.baldini@pi.infn.it)         *
+# *                                                                   *
+# * For the license terms see the file LICENSE, distributed           *
+# * along with this software.                                         *
+# *********************************************************************
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+from e3pipe.__logging__ import logger, abort
+from e3pipe.root.E3InputRootFile import E3InputRootFile
+from e3pipe.root.E3Canvas import E3Canvas
+
+
+
+class E3DataQualityMonitor:
+
+    """ Basic DQM class.
+    """
+
+    def __init__(self, filePath, outputFolder = None):
+        """ Constructor.
+        """
+        self.__InputFile = E3InputRootFile(filePath)
+        self.__OutputFolder = outputFolder
+        self.__AlarmList = []
+
+    def alarms(self):
+        """ Return the list of active alarms.
+        """
+        return self.__AlarmList
+
+    def draw(self, objName, **kwargs):
+        """ Draw a plot from the DST.
+        """
+        _canvas = E3Canvas('c%s' % objName, **kwargs)
+        _rootObject = self.__InputFile.Get(objName)
+        _rootObject.Draw()
+        _canvas.Update()
+        if self.__OutputFolder is not None:
+            _canvas.save(self.__OutputFolder)
+
+    def alarm(self, objName, alarmName, errMin, warnMin, warnMax, errMax,
+              **kwargs):
+        """ Apply an alarm on a ROOT object from the DST.
+        """
+        exec('from e3pipe.dqm.alarm_%s import alarm_%s as alarmClass' %\
+             (alarmName, alarmName))
+        _canvas = E3Canvas('c%s_%s' % (objName, alarmName), **kwargs)
+        _rootObject = self.__InputFile.Get(objName)
+        _rootObject.Draw()
+        _alarm = alarmClass(_rootObject, errMin, warnMin, warnMax, errMax)
+        _alarm.run()
+        _alarm.draw()
+        _canvas.Update()
+        if self.__OutputFolder is not None:
+            _canvas.save(self.__OutputFolder)
+        self.__AlarmList.append(_alarm)
+        logger.info(_alarm)
+
+    def run(self):
+        """ Run the data quality monitoring on a DST file.
+
+        TODO: this should be fully configurable from a file---possibly
+        on a telescope-by-telescope basis.
+        """
+        self.draw('Phi')
+        self.draw('Theta')
+        self.draw('ChiSquare', Logy = True)
+        self.alarm('DeltaTime', 'exp_fit_lambda', 5, 10, 50, 75, Logy = True)
+        self.draw('TimeOfFlight')
+        self.draw('TrackLength')
+        self.alarm('RateTrackEvents', 'y_values', 5, 10, 50, 75)
+        self.alarm('HitMultTop', 'x_average', 0.5, 0.75, 2, 3, Logy = True)
+        self.alarm('HitMultMid', 'x_average', 0.5, 0.75, 2, 3, Logy = True)
+        self.alarm('HitMultBot', 'x_average', 0.5, 0.75, 2, 3, Logy = True)
+        self.alarm('HitMultTotal', 'x_average', 1.5, 2.5, 6, 9, Logy = True)
+        self.alarm('ClusterMultTop', 'x_average', 0.5, 0.75, 2, 3, Logy = True)
+        self.alarm('ClusterMultMid', 'x_average', 0.5, 0.75, 2, 3, Logy = True)
+        self.alarm('ClusterMultBot', 'x_average', 0.5, 0.75, 2, 3, Logy = True)
+        self.alarm('ClusterMultTotal', 'x_average', 1.5, 2.5, 6, 9, Logy = True)
+
+    def createReport(self):
+        """ Create the html report.
+        """
+        pass
+
+
+
+def test(filePath):
+    """ Test function.
+    """
+    monitor = E3DataQualityMonitor(filePath, None)
+    monitor.run()
+
+
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    (opts, args) = parser.parse_args()
+    test(args[0])
