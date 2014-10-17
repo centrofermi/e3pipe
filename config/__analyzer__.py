b13d7927b3847106b21d908c0a2cfe5689905767
Luca Baldini <luca.baldini@pi.infn.it>
2014-10-17 07:21:15 +0200

    Initial import.

---
+++ config/__analyzer__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# *********************************************************************
+# * Copyright (C) 2014 Luca Baldini (luca.baldini@pi.infn.it)         *
+# *                                                                   *
+# * For the license terms see the file LICENSE, distributed           *
+# * along with this software.                                         *
+# *********************************************************************
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+import os
+
+
+""" Path to the official EEE analyzer. 
+"""
+try:
+    E3_ANALYZER  = os.environ['E3_ANALYZER']
+except KeyError:
+    E3_ANALYZER = '/opt/eee/bin/EEE_Analyzer'
+
+
+""" Name of the calibration file that is created on the fly by the analysis
+program.
+"""
+E3_CALIB_FILE_NAME = 'eee_calib.txt'
+
+
+""" Extensions of the output files produced by the analyzer.
+"""
+E3_ANALYZER_OUTPUTS = ['.2tt', '.out', '.sum', '.tim']
+
+
+
+if __name__ == '__main__':
+    for key in dir():
+        if key.startswith('E3'):
+            print '%s = %s' % (key, eval(key))
