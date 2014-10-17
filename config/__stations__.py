b2c923b448df16b2a9372b742948aa84ca628b75
Luca Baldini <luca.baldini@pi.infn.it>
2014-10-17 11:46:53 +0200

    Minor.

--- config/__stations__.py
+++ config/__stations__.py
@@ -24,15 +24,15 @@
 STATION_LIST = ['ALTA-01',
                 'BARI-01',
                 'CAGL-01',
-                #'CAGL-02',
+                'CAGL-02',
                 'CATA-02',
                 'FRAS-02',
                 'GROS-01',
                 'LECC-02',
                 'PISA-01',
-                #'SAVO-01',
+                'SAVO-01',
                 'TORI-04',
-                #'TRAP-01',
+                'TRAP-01',
                 'TRIN-01',
                 'VIAR-02'
             ]
