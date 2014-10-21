e0b43d70fc3fe35a7bb03600a8371d12e0c8cd15
Luca Baldini <luca.baldini@pi.infn.it>
2014-10-22 01:40:00 +0200

    Added station dict.

--- config/__stations__.py
+++ config/__stations__.py
@@ -22,7 +22,53 @@
 
 
 E3_STATION_DICT = {
-
+    'ALTA-01': 1,
+    'ANCO-01': 2,
+    'AREZ-01': 3,
+    'BARI-01': 4,
+    'BOLO-01': 5,
+    'BOLO-02': 6,
+    'BOLO-03': 7,
+    'BOLO-04': 8,
+    'CAGL-01': 9,
+    'CAGL-02': 10,
+    'CAGL-03': 11,
+    'CATA-01': 12,
+    'CATA-02': 13,
+    'CATZ-01': 14,
+    'CERN-01': 15,
+    'CERN-02': 16,
+    'COSE-01': 17,
+    'FRAS-01': 18,
+    'FRAS-02': 19,
+    'GROS-01': 20,
+    'GROT-01': 21,
+    'LAQU-01': 22,
+    'LAQU-02': 23,
+    'LECC-01': 24,
+    'LECC-02': 25,
+    'LECC-03': 26,
+    'LODI-01': 27,
+    'PISA-01': 28,
+    'PARM-01': 29,
+    'PATE-01': 30,
+    'REGG-01': 31,
+    'ROMA-01': 32,
+    'ROMA-02': 33,
+    'SALE-01': 34,
+    'SAVO-01': 35,
+    'SAVO-02': 36,
+    'SAVO-03': 37,
+    'TERA-01': 38,
+    'TORI-01': 39,
+    'TORI-02': 40,
+    'TORI-03': 41,
+    'TORI-04': 42,
+    'TRAP-01': 43,
+    'TREV-01': 44, 
+    'TRIN-01': 45,
+    'VIAR-01': 46,
+    'VIAR-02': 47
 }
 
 
@@ -53,3 +99,5 @@ E3_ACTIVE_STATIONS = [
 if __name__ == '__main__':
     print E3_STATION_DICT
     print E3_ACTIVE_STATIONS
+    print len(E3_STATION_DICT)
+    print len(E3_ACTIVE_STATIONS)
