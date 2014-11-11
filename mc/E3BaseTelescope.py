d399cd2d31e9544e9027f6ff13cae6bdecb31fe7
Luca Baldini <luca.baldini@pi.infn.it>
2014-11-11 10:09:09 +0100

    Minor.

--- mc/E3BaseTelescope.py
+++ mc/E3BaseTelescope.py
@@ -21,23 +21,84 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
+import random
 
-class E3Telescope:
+from e3pipe.mc.E3Point import E3Point
 
-    """ Small class describing the basic geometry of a telescope.
+
+class E3BaseTelescope:
+
+    """ Base class describing the basic geometry of a telescope.
+
+    Here is an extract from the analyzer:
+
+    Y-coordinate is perpendicular to strip direction:
+    Y=0 for strip 1 Y=82 for strip 24
+
+    X-coordinate is along the strip direction and obtained from the
+    time-difference is proportional to (T_left-T_right) which goes from -10 ns
+    to +10 ns:
+    (T_left - T_right)=-9 X=0
+    (T_left - T_right)=+9 X=158
+
+    We use a coordinate system whose origin is in one of the corners of the
+    lowermost RPC plane.
     """
 
-    WIDTH = 100
-    LENGTH = 200
+    WIDTH = 85.56
+    LENGTH = 158.0
+    NUM_STRIPS_PER_PLANE = 24
 
-    def __init__(self, d12, d23, triggerMask = 0b111):
+    def __init__(self, d12 = 50., d23 = 50., phiNorth = 0., triggerMask = 0b111,
+                 latitude = 0., longitude = 0, altitude = 0.):
         """ Constructor.
         """
-        self.__Z = [0, d12, d12 + d23]
+        self.__Z = [0., d12, d12 + d23]
+        self.__PhiNorth = phiNorth
         self.__TriggerMask = triggerMask
-        pass
+        self.__Longitude = longitude
+        self.__Latitude = latitude
+        self.__Altitude = altitude
 
     def z(self, plane):
-        """
+        """ Return the z coordinate of a given plane (0 is the lowermost one,
+        2 is the uppermost one.)
         """
         return self.__Z[plane]
+
+    def ztop(self):
+        """ Return the z coordinate of the uppermost plane.
+        """
+        return self.__Z[2]
+
+    def zmid(self):
+        """ Return the z coordinate of the mid plane.
+        """
+        return self.__Z[1]
+
+    def zbot(self):
+        """ Return the z coordinate of the lowermost plane.
+        """
+        return self.__Z[0]
+
+    def randomPoint(self, plane = 2):
+        """ Return the position of a random hit on one of the planes.
+        """
+        x = random.uniform(0., self.LENGTH)
+        y = random.uniform(0., self.WIDTH)
+        z = self.z(plane)
+        return E3Point(x, y, z)
+
+    def withinActiveArea(self, x, y):
+        """ Return whether a given (x, y) two-dimensional point is within
+        the active area.
+        """
+        return x >= 0 and x <= self.LENGTH and y >= 0 and y <= self.WIDTH
+
+
+
+if __name__ == '__main__':
+    telescope = E3BaseTelescope()
+    p = telescope.randomPoint()
+    print p
+    print telescope.withinActiveArea(p.x(), p.y())
