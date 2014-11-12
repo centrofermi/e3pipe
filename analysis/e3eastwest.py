775e0b629be698808f69a883a56569ac059ae0d4
Luca Baldini <luca.baldini@pi.infn.it>
2014-11-12 12:04:34 +0100

    Minor.

--- analysis/e3eastwest.py
+++ analysis/e3eastwest.py
@@ -21,57 +21,98 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
-import ROOT
-
 from e3pipe.__logging__ import logger
+from e3pipe.root.__ROOT__ import *
+from e3pipe.root.E3H1D import E3H1D
+from e3pipe.root.E3Canvas import E3Canvas
 from e3pipe.dst.E3DstEventChain import E3DstEventChain
+from e3pipe.mc.E3McEventChain import E3McEventChain
+from e3pipe.root.E3Legend import E3Legend
 
 
-def e3eastwest(filePath, northAngle = 56.,
-               baseCut = 'StatusCode == 0 && Theta > 30'):
-    """
-    """
-    chain = E3DstEventChain(filePath)
-    h = ROOT.TH1F('h', '#phi', 100, -180, 180)
-    chain.Project('h', 'Phi', baseCut)
-    return h
+DATA_FILE_PATH = '/data/work/EEE/data/events/SAVO-01-2014-10-28-merged.root'
+MC_FILE_PATH = '/data/work/EEE/data/mc/SAVO-01_mc.root'
+THETA_CUT = 'Theta > 0'
+STATION = 'SAVO-01'
+
+dataChain = E3DstEventChain(DATA_FILE_PATH)
+mcChain = E3McEventChain(MC_FILE_PATH)
+
+htheta = E3H1D('htheta', 'Theta', 75, 0, 90, Minimum = 0,
+               XTitle = '#theta [#circ]')
+hphi = E3H1D('hphi', 'Phi', 75, -180, 180, Minimum = 0,
+             XTitle = '#phi [#circ]')
+
+hthetadata = htheta.Clone('hthetadata')
+hthetadata.SetLineColor(ROOT.kRed)
+hthetadata.SetTitle('Data')
+hphidata = hphi.Clone('hphidata')
+hphidata.SetTitle('Data')
+hphidata.SetLineColor(ROOT.kRed)
+dataCut = '%s && StatusCode == 0 && ChiSquare < 10' % THETA_CUT
+dataChain.Project('hthetadata', 'Theta', dataCut)
+dataChain.Project('hphidata', 'Phi', dataCut)
+hthetadata.Scale(1./hthetadata.GetSumOfWeights())
+hphidata.Scale(1./hphidata.GetSumOfWeights())
+hthetadata.SetMinimum(0)
+hphidata.SetMinimum(0)
 
 
-    expr = 'Phi - %.3f + 360*(Phi - %.3f < -180) - 360*(Phi - %.3f > 180)' %\
-           (northAngle, northAngle, northAngle)
-    chain.SetAlias('PhiNorth', expr)
-    h1 = ROOT.TH1F('h1', '#phi > 0 (East)', 200, 0, 180)
-    h1.SetXTitle('abs(#phi) [#circ from North]')
-    h1.SetLineColor(ROOT.kRed)
-    h2 = ROOT.TH1F('h2', '#phi < 0 (West)', 200, 0, 180)
-    h2.SetLineColor(ROOT.kBlue)
-    cut1 = '%s && PhiNorth > 0' % baseCut
-    chain.Project('h1', 'PhiNorth', cut1)
-    cut2 = '%s && PhiNorth < 0' % baseCut
-    chain.Project('h2', '-PhiNorth', cut2)
-    hratio = h1.Clone('hratio')
-    hratio.Divide(h2)
-    return h1, h2, hratio
-    
+hthetamc = htheta.Clone('hthetamc')
+hthetamc.SetTitle('Monte Carlo')
+hthetamc.SetLineColor(ROOT.kBlue)
+hphimc = hphi.Clone('hphimc')
+hphimc.SetTitle('Monte Carlo')
+hphimc.SetLineColor(ROOT.kBlue)
+mcCut = '%s && Trigger == 1' % THETA_CUT
+mcChain.Project('hthetamc', 'Theta', mcCut)
+mcChain.Project('hphimc', 'Phi', mcCut)
+hthetamc.Scale(1./hthetamc.GetSumOfWeights())
+hphimc.Scale(1./hphimc.GetSumOfWeights())
+hthetamc.SetMinimum(0)
+hphimc.SetMinimum(0)
+
+c1 = E3Canvas('%s_theta_datamc' % STATION)
+hthetadata.Draw()
+hthetamc.Draw('same')
+l1 = E3Legend(0.6, 0.6, entries = [hthetadata, hthetamc])
+l1.Draw()
+c1.Update()
+c1.save()
 
 
-if __name__ == '__main__':
-    from e3pipe.root.E3Canvas import E3Canvas
-    from e3pipe.root.E3Legend import E3Legend
-    from optparse import OptionParser
-    parser = OptionParser()
-    (opts, args) = parser.parse_args()
-    #h1, h2, hratio = e3eastwest(args[0])
-    #c1 = E3Canvas('c1')
-    #h1.Draw()
-    #h2.Draw('same')
-    #l = E3Legend(entries = [h1, h2])
-    #l.Draw()
-    #c1.Update()
-    #c2 = E3Canvas('c2')
-    #hratio.Draw()
-    #c2.Update()
-    h = e3eastwest(args[0])
-    c = E3Canvas('c')
-    h.Draw()
-    c.Update()
+c2 = E3Canvas('%s_phi_datamc' % STATION)
+hphidata.Draw()
+hphimc.Draw('same')
+l2 = E3Legend(0.4, 0.8, entries = [hphidata, hphimc])
+l2.Draw()
+c2.Update()
+c2.save()
+
+
+c3 = E3Canvas('%s_phi_modulation' % STATION)
+hphiratio = hphidata.Clone('hphiratio')
+hphiratio.Divide(hphimc)
+hphiratio.SetLineColor(ROOT.kBlack)
+hphiratio.SetMinimum(0.75)
+hphiratio.SetMaximum(1.25)
+hphiratio.SetYTitle('Data/MC ratio')
+hphiratio.Draw()
+
+def vline(x):
+    y0 = hphiratio.GetMinimum()
+    y1 = hphiratio.GetMaximum()
+    l = ROOT.TLine(x, y0, x, y1)
+    l.SetLineStyle(7)
+    l.Draw()
+    store(l)
+
+for x in [-135, -45, 45, 135]:
+    vline(x)
+
+c3.annotate(0, 0.8, 'North', ndc = False, align = 21)
+c3.annotate(90, 0.8, 'East', ndc = False, align = 21)
+c3.annotate(-90, 0.8, 'West', ndc = False, align = 21)
+
+c3.Update()
+c3.save()
