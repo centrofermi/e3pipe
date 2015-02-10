6e6526f1747d56fc599b56dd6bfa6de0628165ec
Luca Baldini <luca.baldini@pi.infn.it>
2015-02-10 13:46:01 +0100

    Implementing new db register strategy.

--- db/e3registerRun.py
+++ db/e3registerRun.py
@@ -26,13 +26,72 @@ import time
 
 from e3pipe.db.E3RunDbInterface import E3RunDbInterface
 from e3pipe.__logging__ import logger
-from e3pipe.config.__storage__ import E3RawDataInfo
-from e3pipe.dst.__runid__ import uniqueRunIdFromFilePath, runStation, runDate,\
-    runId
 from e3pipe.dst.E3DstHeaderChain import E3DstHeaderChain
+from e3pipe.dst.__runid__ import uniqueRunId
 from e3pipe.root.E3InputRootFile import E3InputRootFile
 from e3pipe.tasks.__exitcodes__ import E3PIPE_EXIT_CODE_SUCCESS,\
     E3PIPE_EXIT_CODE_UNKNOWN
+
+
+def _register(runInfo, db = None, **kwargs):
+    """ Base function.
+    """
+    station = info['Station']
+    date = info['DateString']
+    runId = info['RunNumber']
+    kwargs['unique_run_id'] = uniqueRunId(station, date, runId)
+    dstFilePath = info['DstFilePath']
+    if os.path.exists(dstFilePath):
+        dstLastModTime = time.gmtime(os.path.getmtime(dstFilePath))
+        dstLastModDatetime = time.strftime('%Y-%m-%d %H:%M:%S', dstLastModTime)
+        kwargs['last_processing'] = dstLastModDatetime
+    kwargs['last_update'] = 'NOW()'
+    _closeOnExit = False
+    if db is None:
+        db = E3RunDbInterface()
+        _closeOnExit = True
+    query = 'UPDATE runs SET '
+    for key, value in kwargs.items():
+        query += '%s = %s, ' % (key, value)
+    query = query.strip(' ,')
+    query += ' WHERE station_name = "%s" AND run_date = "%s" AND run_id = %d' %\
+             (station, date, runId)
+    db.execute(query, commit = True)
+    if _closeOnExit:
+        db.close()
+
+def registerSuccess(runInfo, db = None):
+    """ Register a success.
+    """
+    dstFilePath = info['DstFilePath']
+    dstFile = E3InputRootFile(dstFilePath)
+    header = dstFile.Get('Header')
+    header.GetEntry(0)
+    kwargs = {
+        'run_start'             : header.RunStart,
+        'run_stop'              : header.RunStop,
+        'num_events'            : header.NumEvents,
+        'num_hit_events'        : header.NumHitEvents,
+        'num_track_events'      : header.NumTrackEvents,
+        'num_no_hit_events'     : header.NumNoHitEvents,
+        'num_no_hits_events'    : header.NumNoHitsEvent,
+        'num_malformed_events'  : header.NumMalformedEvents,
+        'num_backward_events'   : header.NumBackwardEvent,
+        'processing_status_code': E3PIPE_EXIT_CODE_SUCCESS,
+        'e3pipe_version'        : dstFile.version()
+    }
+    dstFile.Close()
+    _register(runInfo, db, **kwargs)
+
+def registerFailure(runInfo, exitCode, db = None):
+    """ Registed a failure.
+    """
+    kwargs = {
+        'processing_status_code': E3PIPE_EXIT_CODE_SUCCESS,
+        'e3pipe_version'        : dstFile.version()
+    }
+    _register(runInfo, db, **kwargs)
+    
 
 
 
@@ -94,10 +153,3 @@ def e3registerRun(rawFilePath, db):
 
 
 
-if __name__ == '__main__':
-    from optparse import OptionParser
-    parser = OptionParser()
-    (opts, args) = parser.parse_args()
-    db = E3RunDbInterface()
-    for arg in args:
-        e3registerRun(arg, db)
