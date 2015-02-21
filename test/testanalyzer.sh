d8c3fef6b58186cb62800d68bc2402efbe063833
Luca Baldini <luca.baldini@pi.infn.it>
2015-02-21 14:11:51 +0100

    Minor.

--- test/testanalyzer.sh
+++ test/testanalyzer.sh
@@ -3,10 +3,11 @@
 source ~/env.sh
 
 OUTPUT_FOLDER=~/eeedata
+export E3PIPE_TEMP=~/eeetmp
 
 for version in V0 V2.5 V2.7 V3.0 V3.1
 do
     export E3_ANALYZER=/opt/eee/bin/EEE_Analyzer_${version}
     e3analyzer.py -s ${version} $1
-    mv $E3PIPE_TEMP/*.out OUTPUT_FOLDER
+    mv ${E3PIPE_TEMP}/*.out ${OUTPUT_FOLDER}
 done
