91eb90772a794a6e218bc02868e3ce889c9f202d
Luca Baldini <luca.baldini@pi.infn.it>
2015-02-21 13:43:56 +0100

    Initial import.

---
+++ test/testanalyzer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ~/env.sh
+
+OUTPUT_FOLDER=~/eeedata
+
+for version in V0 V2.5 V2.7 V3.0 V3.1
+do
+    export E3_ANALYZER=/opt/eee/bin/EEE_Analyzer_${version}
+    e3analyzer.py -s ${version} $1
+    mv $E3PIPE_TEMP/*.out OUTPUT_FOLDER
+done
