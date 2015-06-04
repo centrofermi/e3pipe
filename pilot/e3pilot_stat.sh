3fbcf6dae85994a0e516257acf9f97ce6d7b2c7e
Luca Baldini <luca.baldini@pi.infn.it>
2015-06-05 01:40:09 +0200

    Initial import.

---
+++ pilot/e3pilot_stat.sh
@@ -0,0 +1,34 @@
+SELECT COUNT(*) AS `Total number of runs` from runs;
+
+SET @PILOT_START_DATE := '2014-10-21';
+SET @PILOT_END_DATE := '2014-11-14';
+
+SELECT * from processing_status_codes;
+
+SELECT processing_status_code, COUNT(*) from runs GROUP BY processing_status_code;
+
+SELECT station_name, COUNT(*) from runs WHERE run_date >= @PILOT_START_DATE AND run_date <= @PILOT_END_DATE GROUP BY station_name;
+
+SELECT COUNT(*) from runs WHERE run_date >= @PILOT_START_DATE AND run_date <= @PILOT_END_DATE;
+
+SELECT SUM(num_events) from runs WHERE run_date >= @PILOT_START_DATE AND run_date <= @PILOT_END_DATE;
+
+SELECT SUM(num_track_events) from runs WHERE run_date >= @PILOT_START_DATE AND run_date <= @PILOT_END_DATE;
+
+#SELECT station_name, COUNT(*) from runs WHERE run_date >= '2015-02-23' AND num_events > 1000 AND (run_stop - run_start) > 60. AND (run_stop - run_start) < 28800. AND num_track_events/num_events > 0.8 AND num_track_events/(run_stop - run_start) > 8. AND num_track_events/(run_stop - run_start) < 80. GROUP BY station_name;
+
+#SELECT COUNT(*) from runs WHERE run_date >= '2015-02-23' AND num_events > 1000 AND (run_stop - run_start) > 60. AND (run_stop - run_start) < 28800. AND num_track_events/num_events > 0.8 AND num_track_events/(run_stop - run_start) > 8. AND num_track_events/(run_stop - run_start) < 80.;
+
+#SELECT station_name, COUNT(*) from runs WHERE run_date >= '2015-02-23' AND num_events > 1000 AND (run_stop - run_start) > 60. AND (run_stop - run_start) < 28800. AND num_track_events/num_events > 0.4 AND num_track_events/(run_stop - run_start) > 4. AND num_track_events/(run_stop - run_start) < 100. GROUP BY station_name;
+
+#SELECT COUNT(*) from runs WHERE run_date >= '2015-02-23' AND num_events > 1000 AND (run_stop - run_start) > 60. AND (run_stop - run_start) < 28800. AND num_track_events/num_events > 0.4 AND num_track_events/(run_stop - run_start) > 4. AND num_track_events/(run_stop - run_start) < 100.;
+
+#SELECT SUM(num_track_events) from runs WHERE run_date >= '2015-02-23';
+
+#SELECT station_name, SUM(num_track_events) from runs WHERE run_date >= '2015-02-23' AND num_events > 1000 AND (run_stop - run_start) > 60. AND (run_stop - run_start) < 28800. AND num_track_events/num_events > 0.8 AND num_track_events/(run_stop - run_start) > 8. AND num_track_events/(run_stop - run_start) < 80. GROUP BY station_name;
+
+#SELECT SUM(num_track_events) from runs WHERE run_date >= '2015-02-23' AND num_events > 1000 AND (run_stop - run_start) > 60. AND (run_stop - run_start) < 28800. AND num_track_events/num_events > 0.8 AND num_track_events/(run_stop - run_start) > 8. AND num_track_events/(run_stop - run_start) < 80.;
+
+#SELECT station_name, SUM(num_track_events) from runs WHERE run_date >= '2015-02-23' AND num_events > 1000 AND (run_stop - run_start) > 60. AND (run_stop - run_start) < 28800. AND num_track_events/num_events > 0.4 AND num_track_events/(run_stop - run_start) > 4. AND num_track_events/(run_stop - run_start) < 100. GROUP BY station_name;
+
+#SELECT SUM(num_track_events) from runs WHERE run_date >= '2015-02-23' AND num_events > 1000 AND (run_stop - run_start) > 60. AND (run_stop - run_start) < 28800. AND num_track_events/num_events > 0.4 AND num_track_events/(run_stop - run_start) > 4. AND num_track_events/(run_stop - run_start) < 100.;
